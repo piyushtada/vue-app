30005c585a95cc6c61460ca1eb1aee16b893cc37
Piyush <piyushtada@gmail.com>
2021-01-19 13:45:10 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <h1> Dc jokers</h1>
+  <ul>
+    <li v-for="he in dcHeroo" :key="he">  {{he}} </li>
+  </ul>
+  <form @submit.prevent="dcHeroo.push(newHero)">
+    <input v-model="newHero" />
+    <button type="submit"> Add hero</button>
+  </form>
+  </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  methods:{
+    addHero(){
+      alert('hooooall');
+    }
+  },
+  data(){
+    return{
+      attribute: "value",
+      isDisabled: 'false',
+      dcHeroo: ["so","what","are"],
+      newHero: "aquamannn",
+    };
+  },
+  
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
